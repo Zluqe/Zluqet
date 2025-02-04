b726242979040a1433bbb40665a90017d83757ed
Person0z <80363995+Person0z@users.noreply.github.com>
2025-02-03 23:28:55 -0600

    Docker + Scaling

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Coming Soon
+# Zluqet
+A open source pastebin service
+
+# How to install
+Run: gunicorn -w 25 -b 0.0.0.0:5000 zluqet:app
--- zluqet.py
+++ zluqet.py
@@ -97,4 +97,4 @@ def page_not_found(e):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=False, threaded=True)
